fea10df2e2a6f83f9031aff410262ef1daacab31
Ramya Kottapalli <rskottap@gmail.com>
2025-07-28 18:22:59 -0500

    Home Manager

---
+++ README.md
@@ -0,0 +1,11 @@
+# Home Manager
+
+Setup dotfiles, configs, shortcuts, preferences etc., via Home Manager.
+
+See `home.nix`.
+Uses prefixes workflow for setting up dotfiles.
+
+```bash
+home-manager switch --flake .#ramya
+```
+
---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "Home Manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations.ramya = home-manager.lib.homeManagerConfiguration {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+      modules = [ ./home.nix ];
+    };
+  };
+}
