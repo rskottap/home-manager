6b8f45a5bd45f41cf1e9d1a5ce5f1dd07b452649
Ramya Kottapalli <rskottap@gmail.com>
2025-07-30 10:25:02 -0500

    If file already exists backup to .backup instead of failing

--- flake.nix
+++ flake.nix
@@ -16,7 +16,12 @@
         system = "x86_64-linux";
         config.allowUnfree = true;
       };
-      modules = [ ./home.nix ];
+      modules = [ 
+        ./home.nix
+        {
+          home-manager.backupFileExtension = "backup";
+        }
+      ];
     };
 
     # For NixOS
