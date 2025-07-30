ee7d160ebd23b84dbecee22278f189ee5d9ed96b
Ramya Kottapalli <rskottap@gmail.com>
2025-07-30 13:05:05 -0500

    Move all the repos to top-level

--- home.nix
+++ home.nix
@@ -3,27 +3,25 @@
 let
   user = "ramya";
   
-  repos = {
-    exec = {
-      src = "https://github.com/thedynamiclinker/exec";
-      dst = "${config.home.homeDirectory}/Desktop/repos/exec";
-    };
-    personal = {
-      src = "https://github.com/rskottap/personal";
-      dst = "${config.home.homeDirectory}/Desktop/repos/personal";
-    };
-    secret = {
-      src = "https://github.com/rskottap/secret";
-      dst = "${config.home.homeDirectory}/Desktop/repos/secret";
-    };
-    shortcuts = {
-      src = "https://github.com/rskottap/shortcuts";
-      dst = "${config.home.homeDirectory}/Desktop/repos/shortcuts";
-    };
-    nixos = {
-      src = "https://github.com/rskottap/nixos";
-      dst = "${config.home.homeDirectory}/Desktop/repos/nixos";
-    };
+  exec = {
+    src = "https://github.com/thedynamiclinker/exec";
+    dst = "${config.home.homeDirectory}/Desktop/repos/exec";
+  };
+  personal = {
+    src = "https://github.com/rskottap/personal";
+    dst = "${config.home.homeDirectory}/Desktop/repos/personal";
+  };
+  secret = {
+    src = "https://github.com/rskottap/secret";
+    dst = "${config.home.homeDirectory}/Desktop/repos/secret";
+  };
+  shortcuts = {
+    src = "https://github.com/rskottap/shortcuts";
+    dst = "${config.home.homeDirectory}/Desktop/repos/shortcuts";
+  };
+  nixos = {
+    src = "https://github.com/rskottap/nixos";
+    dst = "${config.home.homeDirectory}/Desktop/repos/nixos";
   };
 
   desktopDirs = [
@@ -59,40 +57,64 @@ in {
     export PATH="${pkgs.git}/bin:$PATH"
     export PATH="${pkgs.openssh}/bin/:$PATH"
 
-    ${lib.concatStringsSep "\n" (lib.mapAttrsToList (name: r:
+    ${lib.concatStringsSep "\n" [
       ''
-        if [ ! -d "${r.dst}" ]; then
-          git clone "${r.src}" "${r.dst}"
-          echo "✅  Cloned ${name}."
+        if [ ! -d "${exec.dst}" ]; then
+          git clone "${exec.src}" "${exec.dst}"
+          echo "✅  Cloned exec."
         fi
       ''
-    ) repos)}
+      ''
+        if [ ! -d "${personal.dst}" ]; then
+          git clone "${personal.src}" "${personal.dst}"
+          echo "✅  Cloned personal."
+        fi
+      ''
+      ''
+        if [ ! -d "${secret.dst}" ]; then
+          git clone "${secret.src}" "${secret.dst}"
+          echo "✅  Cloned secret."
+        fi
+      ''
+      ''
+        if [ ! -d "${shortcuts.dst}" ]; then
+          git clone "${shortcuts.src}" "${shortcuts.dst}"
+          echo "✅  Cloned shortcuts."
+        fi
+      ''
+      ''
+        if [ ! -d "${nixos.dst}" ]; then
+          git clone "${nixos.src}" "${nixos.dst}"
+          echo "✅  Cloned nixos."
+        fi
+      ''
+    ]}
   '';
 
   home.activation.setupDotfiles = lib.hm.dag.entryAfter ["cloneRepos"] ''
-    ln -svf "${repos.exec.dst}/etc/vimrc" ~/.vimrc
-    ln -svf "${repos.secret.dst}/etc/pypirc" ~/.pypirc
-    ln -svf "${repos.personal.dst}/etc/gitconfig" ~/.gitconfig
+    ln -svf "${exec.dst}/etc/vimrc" ~/.vimrc
+    ln -svf "${secret.dst}/etc/pypirc" ~/.pypirc
+    ln -svf "${personal.dst}/etc/gitconfig" ~/.gitconfig
   '';
 
   home.activation.setupShortcuts = lib.hm.dag.entryAfter ["setupDotfiles"] ''
     export PATH="${pkgs.dconf}/bin:$PATH"
 
-    if [ -f "${repos.shortcuts.dst}/cinnamon/custom-shortcuts-setup" ]; then
-      cd "${repos.shortcuts.dst}/cinnamon/" && bash "./custom-shortcuts-setup"
+    if [ -f "${shortcuts.dst}/cinnamon/custom-shortcuts-setup" ]; then
+      cd "${shortcuts.dst}/cinnamon/" && bash "./custom-shortcuts-setup"
     fi
     
-    if [ -f "${repos.shortcuts.dst}/vscode/vscode-shortcuts-setup" ]; then
-      cd "${repos.shortcuts.dst}/vscode/" && bash "./vscode-shortcuts-setup"
+    if [ -f "${shortcuts.dst}/vscode/vscode-shortcuts-setup" ]; then
+      cd "${shortcuts.dst}/vscode/" && bash "./vscode-shortcuts-setup"
     fi
     echo "✅ Setup Shortcuts."
   '';
 
   # NOTE: On NIXOS simply rm /etc/nixos and symlink it to your own nixos repo (wherever it lives)
   # home.activation.setupNixosSymlinks = lib.hm.dag.entryAfter ["setupShortcuts"] ''
-  #   if [ -d /etc/nixos ] && [ -f "${repos.nixos.dst}/configuration.nix" ]; then
+  #   if [ -d /etc/nixos ] && [ -f "${nixos.dst}/configuration.nix" ]; then
   #     sudo rm -rf /etc/nixos
-  #     sudo ln -svf ${repos.nixos.dst} /etc/nixos
+  #     sudo ln -svf ${nixos.dst} /etc/nixos
   #     echo "✅ Symlinked /etc/nixos"
   #   fi
   # '';
@@ -101,14 +123,14 @@ in {
     enable = true;
     bashrcExtra = ''
       for config_file in \
-        "${repos.exec.dst}/etc/bashrc" \
-        "${repos.personal.dst}/etc/bashrc" \
-        "${repos.secret.dst}/etc/bashrc"; do
+        "${exec.dst}/etc/bashrc" \
+        "${personal.dst}/etc/bashrc" \
+        "${secret.dst}/etc/bashrc"; do
         [ -f "$config_file" ] && source "$config_file"
       done
 
-      if [ -d "${repos.personal.dst}/etc/bash_completion.d" ]; then
-        for f in "${repos.personal.dst}"/etc/bash_completion.d/*; do
+      if [ -d "${personal.dst}/etc/bash_completion.d" ]; then
+        for f in "${personal.dst}"/etc/bash_completion.d/*; do
           [ -f "$f" ] && source "$f"
         done
       fi
@@ -116,13 +138,13 @@ in {
   };
 
   # PATH for interactive shells
-  home.sessionVariables.PATH = "${repos.exec.dst}/bin:${repos.personal.dst}/bin:${repos.secret.dst}/bin:$PATH";
+  home.sessionVariables.PATH = "${exec.dst}/bin:${personal.dst}/bin:${secret.dst}/bin:$PATH";
 
   # PATH for login shells
   home.sessionPath = [
-    "${repos.exec.dst}/bin"
-    "${repos.personal.dst}/bin"
-    "${repos.secret.dst}/bin"
+    "${exec.dst}/bin"
+    "${personal.dst}/bin"
+    "${secret.dst}/bin"
   ];
 
   # ~/.config/gh
